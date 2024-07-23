b57c7e05d2f698646c7022250e9c02368d024c64
村松伽哉 <0zn3260z283295d@ezweb.ne.jp>
2024-07-23 09:27:37 +0900

    最初のコミット

---
+++ src/components/TodoList.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import supabase from "../utils/supabaseClient";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase
+      .from("todo_list")
+      .select("*")
+      .order("inserted_at", { ascending: false });
+
+    if (error) {
+      console.error("Error fetching todos:", error);
+    } else {
+      setTodos(data);
+    }
+  };
+
+  const addTodo = async () => {
+    const { data, error } = await supabase
+      .from("todo_list")
+      .insert([{ todo: newTodo }])
+      .select(); // .select() を追加して挿入されたデータを取得
+
+    if (error) {
+      console.error("Error adding todo:", error);
+    } else {
+      // dataが配列であることを確認し、data[0]が存在するかどうかをチェック
+      if (data && data.length > 0) {
+        setTodos([data[0], ...todos]);
+        setNewTodo("");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ src/utils/supabaseClient.js
@@ -0,0 +1,12 @@
+// src/utils/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key:", supabaseKey);
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
